67144ef9a72a02f0258f195f5de7f09625f1eaa0
Mati9284 <matiaseest5@gmail.com>
2025-06-12 10:30:14 -0300

    Add files via upload

---
+++ L.P MAIN TP4 2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <locale.h>
+using namespace std;
+int main ()
+{
+	setlocale(LC_ALL, "spanish");
+	float num1=0;
+	cout << "Este programa indica una forma rectangular " << endl;
+	cout << "Ingrese número positivo" << endl;
+	cin >> num1;
+	if
+	for (int i=0; i<num1; i++)
+	{
+		cout << "*" << endl;
+		for (int j=0; j<num1; j++)
+		{
+			cout <<  "*";
+		}
+	}
+		cout <<  "*";
+	return 0;
+}
